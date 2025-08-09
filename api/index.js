458ec7bdade08725b4017f52440684e2e1b713ba
Jonatan <jonatan.lucero@gmail.com>
2025-08-09 18:35:07 -0300

    actualizacion de cors

--- api/index.js
+++ api/index.js
@@ -27,7 +27,7 @@ const PORT = process.env.PORT || 3000;
 const corsOptions = {
   origin: [
     "http://localhost:5173",
-    "https://prueba-ujdb.onrender.com/",
+    "https://prueba-ujdb.onrender.com",
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"], // Incluye Authorization aquí
